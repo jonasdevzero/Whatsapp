0afcbb1e4736ac3c92afa5806cd6f19fc271c544
jonasdevzero <jonasdevzero@gmail.com>
2020-09-30 21:09:41 -0300

    initial configs

---
+++ frontend/src/globalStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+`
