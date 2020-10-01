24559f1b58ea228131f9e97e824bca0cbafa85cb
jonasdevzero <jonasdevzero@gmail.com>
2020-10-01 17:31:02 -0300

    Adding Chat component part 1

--- frontend/src/components/Chat/index.jsx
+++ frontend/src/components/Chat/index.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React from 'react';
 
-import { Container } from './styles'
+import Header from '../Header';
+
+import {
+    Container,
+    RoomName,
+    LastMessage,
+    Content,
+    Message,
+    MessageReciver,
+    User,
+    TimeStamp,
+} from './styles';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 
 function Chat() {
+    const getTime = _ => {
+        const hour = new Date().getHours()
+        let minutes = new Date().getMinutes()
+        minutes = minutes > 0 && minutes < 10 ? `0${minutes}` : minutes
+        const timer = hour >= 12 && hour < 24 ? 'PM' : 'AM'
+
+        return `${hour}:${minutes} ${timer}`
+    }
+
     return (
         <Container>
-            CHAT
+            <Header borderBottom>
+                <Header.Avatar />
+                <Header.Info>
+                    <RoomName>Room Name</RoomName>
+                    <LastMessage>Last seen at...</LastMessage>
+                </Header.Info>
+                <Header.Right>
+                    <SearchOutlined />
+                    <AttachFile />
+                    <MoreVert />
+                </Header.Right>
+            </Header>
+            <Content>
+                <Message>
+                    <User>Jonas</User>
+                    Hi devs!
+                    <TimeStamp>{getTime()}</TimeStamp>
+                </Message>
+                <MessageReciver>
+                    <User>Z</User>
+                        Hi devs!
+                    <TimeStamp>{getTime()}</TimeStamp>
+                </MessageReciver>
+            </Content>
         </Container>
-    )
+    );
 }
 
-export default Chat
+export default Chat;
--- frontend/src/components/Chat/styles.js
+++ frontend/src/components/Chat/styles.js
@@ -1,7 +1,62 @@
-import styled from 'styled-components/macro'
+import styled from 'styled-components/macro';
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
     flex: .65;
+`;
+
+export const RoomName = styled.h3`
+    margin-bottom: 3px;
+    font-weight: 500;
+`;
+
+export const LastMessage = styled.p`
+    color: gray;
+`;
+
+export const Content = styled.div`
+    background: url('https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png') repeat center;
+    flex: 1;
+    padding: 30px;
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        width: 0px;
+    }
+`;
+
+export const Message = styled.p`
+    position: relative;
+    font-size: 16px;
+    padding: 25px 10px 10px 10px;
+    width: fit-content;
+    border-radius: 10px;
+    background-color: #fff;
+    margin-bottom: 30px;
+
+
+`;
+
+export const User = styled.span`
+    position: absolute;
+    top: 6px;
+    left: 10px;
+    font-weight: 700;
+    font-size: 13px;
+`;
+
+export const TimeStamp = styled.span`
+    font-size: 12px;
+    margin-left: 20px;
+`
+
+export const MessageReciver = styled(Message)`
+    background-color: #dcf8c6;
+    margin-left: auto;
+    padding: 10px;
+
+    ${User} {
+        display: none;
+    }
 `
