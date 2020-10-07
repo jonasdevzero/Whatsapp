056e53ab09c2653b0e37f6e753888cd908335550
jonasdevzero <jonasdevzero@gmail.com>
2020-10-06 21:37:59 -0300

    improving sidebar structure/ add sidebar container

--- frontend/src/components/Sidebar/styles.js
+++ frontend/src/components/Sidebar/styles.js
@@ -3,45 +3,9 @@ import styled from 'styled-components/macro';
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    flex: .30;
+    flex: .3;
+    position: relative;
 `;
-
-export const Search = styled.div`
-    display: flex;
-    align-items: center;
-    background-color: #f6f6f6;
-    height: 39px;
-    padding: 10px;
-`;
-
-export const SearchContainer = styled.div`
-    display: flex;
-    align-items: center;
-    background-color: #fff;
-    width: 100%;
-    height: 35px;
-    border-radius: 20px;
-
-    .MuiSvgIcon-root {
-        color: gray;
-        padding: 10px;
-    }
-
-    @media (max-width: 500px) {
-        .MuiSvgIcon-root {
-            display: none !important;
-        }
-    }
-`;
-
-export const SearchInput = styled.input`
-    border: none;
-    outline: none;
-    margin-left: 10px;
-    flex-grow: 1;
-    overflow: hidden;
-`;
-
 
 export const Chats = styled.div`
     flex: 1;
---
+++ frontend/src/containers/SidebarContainer.jsx
@@ -0,0 +1,194 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from '../constants/axios';
+import Fuse from 'fuse.js';
+
+import { UserContext } from '../context/userContext';
+import { Header, Sidebar, SidebarChat, Form, Dropdown, Dropside } from '../components';
+
+import { IconButton } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+function SidebarContainer({
+    setCurrentRoom,
+    rooms,
+    setRooms,
+    profileDropdown,
+    setProfileDropdown,
+    hideDropdown
+}) {
+    const { user, setUser } = useContext(UserContext);
+
+    const [profileContainer, setProfileContainer] = useState(false);
+    const [newRoomContainer, setNewRoomContainer] = useState(false);
+
+    const [name, setName] = useState(user.name);
+    const [imageUrl, setImageUrl] = useState(user.imageUrl);
+
+    const [newRoomName, setNewRoomName] = useState('');
+    const [newRoomImage, setNewRoomImage] = useState('');
+
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([])
+
+    const history = useHistory();
+
+    useEffect(_ => {
+        const fuse = new Fuse(rooms, { keys: ['name'] })
+        const results = fuse.search(search).map(({ item }) => item)
+
+        if (results.length > 0) {
+            setSearchResults(results);
+        } else {
+            setSearchResults([])
+        }
+
+    }, [search, rooms]);
+
+    function signOut(e) {
+        e.preventDefault();
+
+        localStorage.removeItem('authUser');
+        setUser(undefined);
+        history.push('/');
+    };
+
+    async function updateUser(e) {
+        e.preventDefault();
+
+        await axios.post('/api/user/update', { username: user.username, name, imageUrl })
+            .then(resp => {
+                localStorage.setItem('authUser', JSON.stringify(resp.data.userUpdated))
+                setUser(resp.data.userUpdated)
+            })
+        setProfileContainer(false);
+    };
+
+    async function createRoom(e) {
+        e.preventDefault();
+
+        await axios.post('/api/rooms/create', {
+            name: newRoomName,
+            image: newRoomImage,
+            createdBy: user.username
+        });
+        await axios.get('/api/rooms/get')
+            .then(resp => {
+                setRooms(resp.data)
+                setCurrentRoom(resp.data[resp.data.length - 1])
+            });
+
+        setProfileContainer(false);
+        setNewRoomContainer(false);
+
+        setNewRoomName('');
+        setNewRoomImage('');
+    };
+
+    return (
+        <Sidebar onClick={_ => hideDropdown()}>
+            <Dropside showContainer={profileContainer} onClick={_ => hideDropdown()}>
+
+                <Dropside.TitleContainer>
+                    <Dropside.Title>
+                        <ArrowBackIcon onClick={_ => setProfileContainer(false)} />
+                            Profile
+                        </Dropside.Title>
+                </Dropside.TitleContainer>
+
+                <Dropside.PictureContainer>
+                    <Dropside.Picture src={user?.imageUrl} />
+                </Dropside.PictureContainer>
+
+                <Form onSubmit={updateUser} backgroundColor="none">
+                    <Form.Label>Your name</Form.Label>
+                    <Form.DropsideInput value={name} onChange={e => setName(e.target.value)} />
+
+                    <Form.Label>You image URL</Form.Label>
+                    <Form.DropsideInput value={imageUrl} onChange={e => setImageUrl(e.target.value)} />
+
+                    <Form.Submit>Change</Form.Submit>
+                </Form>
+
+            </Dropside>
+            <Dropside showContainer={newRoomContainer} onClick={_ => hideDropdown()}>
+                <Dropside.TitleContainer>
+                    <Dropside.Title>
+                        <ArrowBackIcon onClick={_ => setNewRoomContainer(false)} />
+                            New chat
+                    </Dropside.Title>
+                </Dropside.TitleContainer>
+
+                <Form onSubmit={createRoom} backgroundColor="none">
+                    <Form.Label>Chat name</Form.Label>
+                    <Form.DropsideInput value={newRoomName} onChange={e => setNewRoomName(e.target.value)} />
+
+                    <Form.Label>Image Url</Form.Label>
+                    <Form.DropsideInput value={newRoomImage} onChange={e => setNewRoomImage(e.target.value)} />
+
+                    <Form.Submit>Create</Form.Submit>
+                </Form>
+            </Dropside>
+
+            <Header padding="0">
+
+                <IconButton onClick={_ => setProfileContainer(true)}>
+                    <Header.Picture src={user?.imageUrl} />
+                </IconButton>
+
+                <Header.Right>
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton onClick={_ => setNewRoomContainer(true)}>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton onClick={_ => setProfileDropdown(!profileDropdown)}>
+                        <MoreVertIcon />
+                    </IconButton>
+                </Header.Right>
+
+                <Dropdown showDropdown={profileDropdown}>
+                    <Dropdown.Item onClick={_ => setProfileContainer(true)}>
+                        Profile
+                        </Dropdown.Item>
+                    <Dropdown.Item onClick={e => signOut(e)}>
+                        Log out
+                        </Dropdown.Item>
+                </Dropdown>
+
+            </Header>
+
+            <Form>
+                <Form.Search>
+                    <SearchOutlined />
+                    <Form.SearchInput
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                        placeholder="Search or start new chat"
+                        type="text"
+                    />
+                </Form.Search>
+            </Form>
+
+            <Sidebar.Chats>
+                {
+                    searchResults.length > 0 ?
+                        searchResults.map(room => ( // filtered rooms
+                            <SidebarChat room={room} key={room._id} onClick={_ => setCurrentRoom(room)} />
+                        ))
+                        :
+                        rooms.map(room => ( // All rooms
+                            <SidebarChat room={room} key={room._id} onClick={_ => setCurrentRoom(room)} />
+                        ))
+                }
+            </Sidebar.Chats>
+        </Sidebar>
+    );
+}
+
+export default SidebarContainer;
