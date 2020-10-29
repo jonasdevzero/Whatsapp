6b4fce4728b4325f776c90db300c75b6d0e27ba0
jonasdevzero <jonasdevzero@gmail.com>
2020-10-28 23:48:25 -0300

    Fixing bugs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,13 +11,12 @@ router.post('/api/users/login', UsersController.login);
 router.post('/api/users/auth', UsersController.auth, UsersController.show);
 router.put('/api/users', UsersController.update);
 
-router.get('api/messages/:room_id', MessagesController.index);
-router.post('api/messages', MessagesController.create);
-router.delete('api/messages', MessagesController.delete);
+router.get('/api/messages', MessagesController.index);
+router.post('/api/messages', MessagesController.create);
 
 router.get('/api/rooms', RoomsController.index);
 router.post('/api/rooms', RoomsController.create);
 router.delete('/api/rooms', RoomsController.delete);
-router.put('/api/rooms', RoomsController.update);
+router.put('/api/rooms/:id', RoomsController.update);
 
 module.exports = router;
