35ba6b4a72aef501895995bcca7bb0b4caf06fbb
jonasdevzero <jonasdevzero@gmail.com>
2020-10-29 14:30:48 -0300

    updating rooms controller

--- backend/src/controllers/rooms.js
+++ backend/src/controllers/rooms.js
@@ -1,12 +1,13 @@
 const Rooms = require('../models/rooms');
-const { deleteMessages } = require('./messages');
+const Messages = require('../models/messages');
+const { where } = require('../models/rooms');
 
 module.exports = {
     async index(req, res) {
         try {
             const rooms = await Rooms.find();
     
-            return res.status(200).json(rooms);
+            return res.status(200).json({ rooms });
         } catch (err) {
             return res.status(500).json({ error: 'Error on server' });
         };
@@ -19,46 +20,49 @@ module.exports = {
             return res.json({ error: 'Choose a name for the Room' });
     
         try {
-            const newRoom = await Rooms.create(req.body);
+            await Rooms.create(req.body);
+            const rooms = await Rooms.find(); 
     
-            return res.status(200).json({ newRoom });
+            return res.status(200).json({ rooms });
         } catch (err) {
             return res.status(500).json({ error: 'Error on server' });
         };
     },
 
     async update(req, res) {
-        const { username, room, data } = req.body;
-    
-        if (username !== room.createdBy) 
-            return res.json({ error: 'It is not possible change the data of this room' });
-    
-        const _id = room._id
-        
+        const { username, _id, data } = req.body;
     
         try {
-            const roomUpdated = await Rooms.findOneAndUpdate({ _id: _id }, data, { new: true })
-            return res.status(200).json({ roomUpdated })
+            const room = await Rooms.findOne({ _id })
+            if (username !== room.createdBy) {
+                return res.json({ error: 'It is not possible change the data of this room' });
+            };
+
+            await Rooms.findOneAndUpdate({ _id }, data);
+
+            const rooms = await Rooms.find();
+            return res.status(200).json({ rooms });
         } catch (err) {
-            return res.status(500).json({ error: 'Error on server' })
+            return res.status(500).json({ error: 'Error on server' });
         };
     },
 
     async delete(req, res) {
-        const { room, username } = req.body;
-        const _id = room._id
-    
-        if (username !== room.createdBy)
-            return res.json({ error: 'Only the creator that room can delete it' })
+        const { _id, username } = req.query;
     
         if (_id === '5f78e066e4720631e41f40c7' || _id === '5f790dcfc6eb7b2688688112') 
-            return res.json({ error: 'It is not possible to delete this room'})
+            return res.json({ error: 'It is not possible to delete this room'});
         
         try {
-            deleteMessages(_id )
-            const roomDeleted = await Rooms.findByIdAndDelete({ _id });
-    
-            return res.status(200).json({ roomDeleted });
+            const room = await Rooms.findOne({ _id });
+            if (username !== room.createdBy) {
+                return res.json({ error: 'Only the creator that room can delete it' });
+            };
+            
+            await Messages.deleteMany({ room_id: _id })
+            await Rooms.findByIdAndDelete(_id);
+
+            return res.status(200);
         } catch (err) {
             return res.status(500).json({ error: 'Error on server' });
         };
